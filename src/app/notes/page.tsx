186cf8420922b49486be404823c04f625f732a46
Sergiu Stan <sergiustan007@gmail.com>
2023-07-03 21:30:10 +0300

    solved errors for deployment

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -3,19 +3,39 @@ import React from "react";
 import { auth } from "@clerk/nextjs";
 
 import SearchNotes from "@/components/SearchNotes";
+import { driver } from "@/lib/neo4j";
 
 type Props = {};
 
 const getNotes = async (userId: string) => {
-  const notesData = await fetch(
-    `http://localhost:3000/api/notes?userId=${userId}`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
+  // const notesData = await fetch(
+  //   `http://localhost:3000/api/notes?userId=${userId}`,
+  //   {
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   }
+  // );
+  // return await notesData.json();
+  const session = driver.session();
+  const resNotes = await session.executeRead((tx) =>
+    tx.run(
+      `MATCH (u:User {userId: $userId})-[r:HAS_NOTE]->(n:Note)
+    OPTIONAL MATCH (n)-[:TAGGED_IN]->(t:Tag)
+    WITH COLLECT(DISTINCT t.name) AS tags, n
+   WITH {title: n.title, content: n.content, id: n.id, updated_at: apoc.date.toISO8601(datetime(n.updated_at).epochMillis, "ms"), tags: tags} as note
+   WITH COLLECT(note) as notes
+   OPTIONAL MATCH (u:User {userId: $userId})-[:HAS_TAG]->(t:Tag)
+    WITH COLLECT(t.name) as tags, notes
+    RETURN {notes: notes, tags: tags} as notes
+    `,
+      { userId }
+    )
   );
-  return await notesData.json();
+
+  const notes = resNotes.records.map((r) => r.get("notes"));
+  await session.close();
+  return notes[0];
 };
 
 const page = async ({}: Props) => {

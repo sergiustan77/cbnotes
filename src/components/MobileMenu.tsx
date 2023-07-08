1075f4f2095fec626399048545cc5ad54783fa51
Sergiu Stan <sergiustan007@gmail.com>
2023-07-08 04:53:24 +0300

    fixed new note button in mobile menu

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -8,7 +8,7 @@ import {
 import { Button, buttonVariants } from "./ui/button";
 import { Label } from "./ui/label";
 import Link from "next/link";
-import { UserButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import { UserButton, SignedIn, SignedOut, useAuth } from "@clerk/nextjs";
 import {
   Sheet,
   SheetClose,
@@ -22,10 +22,29 @@ import {
 import React from "react";
 
 import { cn } from "@/lib/utils";
+import randomUUID from "@/lib/randomUUID";
+import { useRouter } from "next/navigation";
 
 type Props = {};
 
 const MobileMenu = (props: Props) => {
+  const { userId } = useAuth();
+  const router = useRouter();
+  const newNote = async () => {
+    const id = randomUUID();
+    const res = await fetch("/api/notes/new", {
+      method: "POST",
+      body: JSON.stringify({
+        userId: userId as string,
+        title: "",
+        content: "",
+        noteContentText: "",
+        id: id,
+      }),
+    }).then(() => {
+      router.push(`/notes/${id}`);
+    });
+  };
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -71,19 +90,14 @@ const MobileMenu = (props: Props) => {
                 </Link>
               </SheetClose>
               <SheetClose asChild>
-                <Link
-                  type="submit"
-                  href={"/notes/new-note"}
-                  className={cn(
-                    buttonVariants({
-                      variant: "default",
-                      size: "default",
-                    }),
-                    "flex w-full gap-2 "
-                  )}
+                <Button
+                  onClick={newNote}
+                  variant="default"
+                  size="default"
+                  className="w-full"
                 >
-                  <PlusIcon className="w-6 h-6" /> New note
-                </Link>
+                  <PlusIcon />
+                </Button>
               </SheetClose>
             </div>{" "}
           </SignedIn>
